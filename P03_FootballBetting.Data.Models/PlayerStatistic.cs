19612c49a3f5d770b59bd9e850fe3306e8e22348
Todor <84274078+TLizhev@users.noreply.github.com>
2021-11-01 10:11:26 +0000

    Add models

---
+++ P03_FootballBetting.Data.Models/PlayerStatistic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_FootballBetting.Data.Models
+{
+   public class PlayerStatistic
+    {
+        
+        // Composite Primary Key
+
+        public int GameId { get; set; }
+        public virtual Game Game { get; set; }
+        public int PlayerId { get; set; }
+        public virtual Player Player { get; set; }
+        public byte ScoredGoals { get; set; }
+
+        public byte Assists { get; set; }
+
+        public byte MinutesPlayed { get; set; }
+
+    }
+}
---
+++ P03_FootballBetting.Data/Migrations/20210709092916_asaas.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_FootballBetting.Data.Migrations
+{
+    public partial class asaas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Result",
+                table: "Games",
+                type: "varchar(7)",
+                unicode: false,
+                maxLength: 7,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "varchar(7)",
+                oldUnicode: false,
+                oldMaxLength: 7,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Result",
+                table: "Games",
+                type: "varchar(7)",
+                unicode: false,
+                maxLength: 7,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(7)",
+                oldUnicode: false,
+                oldMaxLength: 7);
+        }
+    }
+}
